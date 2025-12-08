572c8d4a86297907e4a4a61ddeaf135d3e53c6cb
thakurishuraj <thakurishuraj4608@gmail.com>
2025-12-08 11:57:10 +0530

    Add files via upload

--- divide.c
+++ divide.c
@@ -5,7 +5,7 @@ int main(){
 int a, b;
 printf("Enter the value of a and b : ");
 scanf("%d %d", &a, &b);
-printf("The multiplication of %d and %d is %d.", a, b, a*b);
+printf("The division of %d and %d is %d.", a, b, a/b);
 
 return 0;
 
